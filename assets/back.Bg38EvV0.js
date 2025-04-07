5c3a4f95d15af5794949e8fb37decd673c7823cf
shifulong7 <shifulong7@users.noreply.github.com>
2025-04-07 03:48:40 +0000

    deploy: 420269b8b48706d48183cedf09323954e5a874a0

---
+++ assets/back.Bg38EvV0.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.ueC3T5Rj.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
